eb540fc2c4710cf71fcb88194ee7e0f95f70ae6c
Dante <dante.a.sanchez@gmail.com>
2022-08-18 21:52:21 -0400

    Make vote percentages visible

--- client/js/consensus.js
+++ client/js/consensus.js
@@ -1,14 +1,12 @@
 let connection_status = document.getElementById("connection-status-container")
-let api_div = document.getElementById("api-address");
-let rpc_div = document.getElementById("rpc-address");
-let version_div = document.getElementById("version-field");
+let version_div = document.getElementById("version");
 let block_height_div = document.getElementById("block-height");
-let prevotes_ratio_div = document.getElementById("prevotes-ratio");
-let prevotes_vp_div = document.getElementById("prevotes-voting-power");
+let prevotes_ratio_div = document.getElementById("prevotes-percentage");
 let prevotes_validators = document.getElementById("prevotes-validators");
-let precommits_ratio_div = document.getElementById("precommits-ratio");
-let precommits_vp_div = document.getElementById("precommits-voting-power");
+let precommits_ratio_div = document.getElementById("precommits-percentage");
 let precommits_validators = document.getElementById("precommits-validators");
+let pv_bar_box = document.getElementById('prevotes-bar-box');
+let pc_bar_box = document.getElementById('precommits-bar-box');
 
 var wsProto = "ws://";
 if (window.location.protocol === "https:") {
@@ -35,6 +33,25 @@ function populate_validators(monikers) {
     });
 }
 
+function setProgress(box, progress) {
+    // Add the relevant number of bars to the progress box
+    while (box.firstChild) {
+        box.removeChild(box.firstChild);
+    }
+
+    let vote_passed = ''
+    if (progress >= 67) {
+        vote_passed = '_pass';
+    }
+
+    for (let i=0; i< progress; ++i) {
+        bar = document.createElement('div');
+        bar.className = 'progress_bar'+vote_passed;
+        box.appendChild(bar)
+    };
+
+};
+
 wso.onopen = function (evt) {
     connection_status.style.display = "none";
 }
@@ -46,10 +63,6 @@ wso.onclose = function (evt) {
 wso.onmessage = async function (event) {
     let data = JSON.parse(await event.data);
 
-    if (data.hasOwnProperty('data_sources')) {
-        api_div.textContent = data['data_sources']['api'];
-        rpc_div.textContent = data['data_sources']['rpc'];
-    };
     if (data.hasOwnProperty('monikers')) {
         populate_validators(data['monikers']);
     };
@@ -62,12 +75,9 @@ wso.onmessage = async function (event) {
         block_height_div.textContent = block_height;
     };
     if (data.hasOwnProperty('pv_percentage')) {
-        let pv_percentage = data['pv_percentage'];
-        prevotes_ratio_div.textContent = pv_percentage;
-    };
-    if (data.hasOwnProperty('pv_voting_power')) {
-        let pv_voting_power = data['pv_voting_power'];
-        prevotes_vp_div.textContent = pv_voting_power;
+        let pv_percentage = parseInt(data['pv_percentage']);
+        prevotes_ratio_div.textContent = pv_percentage + '%';
+        setProgress(pv_bar_box, pv_percentage); 
     };
     if (data.hasOwnProperty('pv_list') && data['pv_list'].length > 0) {
         let prevote_list = data['pv_list'];
@@ -81,12 +91,9 @@ wso.onmessage = async function (event) {
         };
     };
     if (data.hasOwnProperty('pc_percentage')) {
-        let pc_percentage = data['pc_percentage'];
-        precommits_ratio_div.textContent = pc_percentage;
-    };
-    if (data.hasOwnProperty('pc_voting_power')) {
-        let pc_voting_power = data['pc_voting_power'];
-        precommits_vp_div.textContent = pc_voting_power;
+        let pc_percentage = parseInt(data['pc_percentage']);
+        precommits_ratio_div.textContent = pc_percentage + '%';
+        setProgress(pc_bar_box, pc_percentage);
     };
     if (data.hasOwnProperty('pc_list') && data['pc_list'].length > 0) {
         let precommit_list = data['pc_list'];
@@ -99,5 +106,5 @@ wso.onmessage = async function (event) {
             }
         };
     };
-
 };
+
